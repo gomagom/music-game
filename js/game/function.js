d35c0d9d398caefda2fbcf2a1e5b4c2807f05ab8
gomagom <s20t331@stu.kagawa-u.ac.jp>
2021-06-10 00:20:31 +0900

    ゲーム本体側のCSVインポート実装

--- js/game/function.js
+++
@@ -1,8 +0,0 @@
-// function getLanePosition(laneNo) {
-//     return (CANVAS_W - LANE.width * 4 - LANE.margin * 3 + LANE.width) / 2 + (LANE.width + LANE.margin) * laneNo;
-// }
-
-// function getLaneKey(laneNo) {
-//     const key = ['d', 'f', 'j', 'k'];
-//     return key[laneNo];
-// }
